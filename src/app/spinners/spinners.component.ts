83aa0b3da2cd058c778fc622a496cbc8a3bc19e3
Fe14080 <lipe.cintran1408@gmail.com>
2022-04-28 20:49:27 -0300

    2 files modified

--- src/app/spinners/spinners.component.ts
+++ src/app/spinners/spinners.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-spinners',
   templateUrl: './spinners.component.html',
-  styleUrls: ['./spinners.component.css'],
 })
 export class SpinnersComponent implements OnInit {
   @Input() cor: string = 'dark';
