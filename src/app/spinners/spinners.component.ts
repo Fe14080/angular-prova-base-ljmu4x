5652140ddc7a62c7663ce4eed71ee30a7b328bfd
Fe14080 <lipe.cintran1408@gmail.com>
2022-04-28 20:46:12 -0300

    3 files modified

--- src/app/spinners/spinners.component.ts
+++ src/app/spinners/spinners.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spinners',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spinners.component.css'],
 })
 export class SpinnersComponent implements OnInit {
+  @Input() cor: string = 'dark';
+  @Input() size: string = '';
+  @Input() grow: boolean = false;
   constructor() {}
 
   ngOnInit() {}
